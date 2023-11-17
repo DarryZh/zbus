9b6b9b03fc8f0f881aa172055ef1250a499896fa
Darryzh <darryzh@163.com>
2023-11-17 16:16:44 +0800

    [config] rm zbus_config impl, add template

--- config/zbus_config_template.h
+++ config/zbus_config_template.h
@@ -1,8 +1,9 @@
 #ifndef __ZBUS_CONFIG_H__
 #define __ZBUS_CONFIG_H__
 
-#define CONFIG_ZBUS_POSIX
+// #define CONFIG_ZBUS_POSIX
 // #define CONFIG_ZBUS_CMSIS_RTOS2
+
 #if ((!defined CONFIG_ZBUS_POSIX) && (!defined CONFIG_ZBUS_CMSIS_RTOS2))
 #define CONFIG_ZBUS_POSIX
 #endif

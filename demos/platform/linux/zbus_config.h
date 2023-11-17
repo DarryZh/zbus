a32424cd2e4e3f13d56481a8eec23558d01e7aa8
Darryzh <darryzh@163.com>
2023-11-17 16:57:44 +0800

    [demos][platform][linux] add linux config

---
+++ demos/platform/linux/zbus_config.h
@@ -0,0 +1,16 @@
+#ifndef __ZBUS_CONFIG_H__
+#define __ZBUS_CONFIG_H__
+
+#define CONFIG_ZBUS_POSIX
+// #define CONFIG_ZBUS_CMSIS_RTOS2
+
+#if ((!defined CONFIG_ZBUS_POSIX) && (!defined CONFIG_ZBUS_CMSIS_RTOS2))
+#define CONFIG_ZBUS_POSIX
+#endif
+
+#define CONFIG_ZBUS_CHANNEL_NAME
+#define CONFIG_ZBUS_OBSERVER_NAME
+
+#define __ASSERT(test, fmt, ...) { }
+
+#endif
